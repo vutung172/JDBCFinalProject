bb9672c096a6677bfec2a713a1cd9e297166e7ae
vutung172 <vuthanhtung1721991@gmail.com>
2024-03-02 11:58:30 +0900

    commit 240302

---
+++ src/com/module3/util/Annotation/Auth.java
@@ -0,0 +1,11 @@
+package com.module3.util.Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Auth {
+}
